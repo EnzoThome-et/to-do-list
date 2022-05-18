f11a6ce8a8b5ecf1ff7ccbb0035762a023dd37cf
enzo <enzo@pop-os.localdomain>
2022-05-18 19:19:54 -0300

    refactor: taskController

--- backend/src/controllers/tasks.ts
+++ backend/src/controllers/tasks.ts
@@ -6,9 +6,12 @@ const create = async (req: Request, res: Response) => {
 	try {
 		const data = req.body;
 		const task = await tasksService.create(data);
+		if ('error' in task) {
+			return res.status(400).json(task);
+		}
 		return res.status(201).json(task);
 	} catch (error) {
-		console.log(error);
+		return res.status(500).json(error);
 	}
 };
 
