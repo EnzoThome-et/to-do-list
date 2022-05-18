f212e8372bf5c520d408356053ca65db0286695e
enzo <enzo@pop-os.localdomain>
2022-05-18 18:58:52 -0300

    feat: connection with db

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,12 @@
 import express, { Application } from 'express';
 import 'dotenv/config';
 import * as tasksController from './src/controllers/tasks';
+import connectToDatabase from './connection';
 
 const app: Application = express();
 const port = process.env.PORT || 3001;
+
+connectToDatabase();
 
 app.use(express.json());
 
