0a2362c599828459b9d093a778361f3f4fae4ae3
enzo <enzo@pop-os.localdomain>
2022-05-19 19:11:14 -0300

    refactor: change SearchBar name to Form

---
+++ frontend/src/components/Form.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { HookForm } from '../interface/hookForm';
+
+
+const Form = (props: HookForm) => {
+	const {register, errors, handleSubmit} = props;
+	const [result, setResult] = useState({});
+	return (
+		<form className="w-50 p-3" onSubmit={handleSubmit(async (data) => {
+			await axios({
+				method: 'post',
+				url: 'http://localhost:3001/tasks',
+				data
+			});
+			const result = await axios.get('http://localhost:3001/tasks');
+			console.log(result.data);
+			setResult(result.data);
+		})}>
+			<div className="form-outline mb-4 d-flex">
+				<input type="text" className="form-control" id="datatable-search-input" {...register('text', { required: true })}/>
+				{errors.text?.type === 'required' && <span>Campo obrigatório!</span>}
+				<select className="form-select w-50" {...register('status', { required: true })}>
+					<option value="">Select...</option>
+					<option value="pending">Pendente</option>
+					<option value="inProgress">Em Andamento</option>
+					<option value="ready">Pronto</option>
+				</select>
+				{errors.status?.type === 'required' && <span>Selecione uma opção!</span>}
+				<button type="submit" className="btn btn-success w-100">Create Task</button>
+			</div>
+			
+		</form>	
+	);
+};
+
+export default Form;
