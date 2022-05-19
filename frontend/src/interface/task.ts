195a6aa7a335f4948fd91f04b68d1a19c90aa188
enzo <enzo@pop-os.localdomain>
2022-05-19 19:31:54 -0300

    refactor: task interface

--- frontend/src/interface/task.ts
+++ frontend/src/interface/task.ts
@@ -1,5 +1,14 @@
+import { Dispatch, SetStateAction } from 'react';
+
 export interface Task {
+    _id: string
     text: string
     status: string
     createdAt: string
+    updatedAt: string
+}
+
+export interface TaskContextType {
+    tasks: Task[]
+    setTasks: Dispatch<SetStateAction<never[]>>
 }
