08f65d1ba4b64ea44842ff23b4c04d085e036827
enzo <enzo@pop-os.localdomain>
2022-05-19 19:59:14 -0300

    fix: ts alert on provider

--- frontend/src/context/provider.tsx
+++ frontend/src/context/provider.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Context from './context';
+import { Task } from '../interface/task';
 
 const AppProvider = ({ children } : {children: React.ReactNode}) => {
-	const [tasks, setTasks] = useState([]);
+	const [tasks, setTasks] = useState<Task[]>([]);
 
 	const defaultState = {
 		tasks,
