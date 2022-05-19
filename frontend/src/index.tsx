5173b78483e94e1bb94df534b841cd2dead9044b
enzo <enzo@pop-os.localdomain>
2022-05-19 19:47:56 -0300

    feat: add provider on index.ts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import AppProvider from './context/provider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<AppProvider>
+			<App />
+		</AppProvider>
 	</React.StrictMode>
 );
 
