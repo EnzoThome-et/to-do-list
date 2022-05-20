84c916096e456fef8808d60cb8bbb2a786b39f67
enzo <enzo@pop-os.localdomain>
2022-05-20 09:32:17 -0300

    fix: types on MainPage

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import SearchBar from '../components/SearchBar';
+import React, {useContext} from 'react';
+import Form from '../components/Form';
 import Table from '../components/Table';
+import { useForm } from 'react-hook-form';
+import Context from '../context/context';
+import { Tasks } from '../interface/task';
 
 const MainPage = () => {
+	const { register, formState: { errors }, handleSubmit } = useForm();
+	const {tasks} = useContext(Context) as Tasks;
+	
 	return (
 		<main className="d-flex  flex-column justify-content-center align-items-center">
 			<h1 className="text-center"> Todo List </h1>
-			<SearchBar/>
-			<Table />
+			<Form handleSubmit={handleSubmit} register={register} errors={errors}/>
+			<Table tasks={tasks}/>
 		</main>
 	);
 };
